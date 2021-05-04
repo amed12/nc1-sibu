6c2bec7281d039e47c960dc826e3350c7162d651
Achmad Fathullah <fathullah@qiscus.co>
2021-05-04 11:07:40 +0700

    first commit

---
+++ sibu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sibu
+//
+//  Created by Achmad Fathullah on 04/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
