ac3905c209cba6363793f72f73ba4137b23636ae
Achmad Fathullah <fathullah@qiscus.co>
2021-05-05 02:20:46 +0700

    add home ui

---
+++ sibu/View/home/homeCell/EmptyStateViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmptyStateViewCell.swift
+//  sibu
+//
+//  Created by Achmad Fathullah on 05/05/21.
+//
+
+import UIKit
+
+class EmptyStateViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
