2d324a2aa38bd20b8250e8c65f5d722bd98a74b2
Achmad Fathullah <fathullah@qiscus.co>
2021-05-05 09:12:22 +0700

    add ui empty state

--- sibu/Controller/HomeController.swift
+++ sibu/Controller/HomeController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class HomeController: UIViewController {
 
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    var isRecommendationEmpty = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
+    
+    func setupView()  {
+        homeTableView.register(UINib(nibName: "EmptyStateViewCell", bundle: nil), forCellReuseIdentifier: "emptyHomeCell")
+    }
 
 }
 
+extension HomeController :UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = homeTableView.dequeueReusableCell(withIdentifier: "emptyHomeCell", for: indexPath) as? EmptyStateViewCell
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 350
+    }
+    
+    
+}
